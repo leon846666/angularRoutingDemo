1140adb3c01ca9b6653aaebd7a3a8b738b1277b8
leon846666 <leon846666@gmail.com>
2018-06-27 14:39:53 -0400

    angular routing demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user',
+    component:UserComponent
+  },
+  {
+    path:'news',
+    component:NewsComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },{
+    // default 
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },{
+    // 404
+    path:'**',
+    component:HomeComponent
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
